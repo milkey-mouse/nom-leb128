dbb6e52a3901626ee8b00fc801a30e46b2e00fca
Milkey Mouse <milkeymouse@meme.institute>
2020-06-24 01:21:58 -0700

    Move internal LEB128 encoder into tests module

--- src/lib.rs
+++ src/lib.rs
@@ -13,14 +13,9 @@ use std::{
 const NEED_ONE: nom::Needed = Needed::Size(unsafe { NonZeroUsize::new_unchecked(1) });
 
 /// Maximum LEB128-encoded size of an integer type
-const fn _leb128_size<T>() -> usize {
+const fn leb128_size<T>() -> usize {
     let bits = size_of::<T>() * 8;
     (bits + 6) / 7 // equivalent to ceil(bits/7) w/o floats
-}
-
-#[cfg(fuzzing)]
-pub const fn leb128_size<T>() -> usize {
-    _leb128_size::<T>()
 }
 
 macro_rules! impl_generic_leb128 {
@@ -42,7 +37,7 @@ macro_rules! impl_generic_leb128 {
                     res |= (byte as $int_ty) << shift;
                     $post(&mut res, shift, byte);
                     return Ok((input.slice(pos + 1..), res));
-                } else if pos == _leb128_size::<$int_ty>() - 1 {
+                } else if pos == leb128_size::<$int_ty>() - 1 {
                     return Err(nom::Err::Error(E::add_context(
                         input.clone(),
                         concat!("LEB128 integer is too big to fit in ", $int_name),
@@ -82,6 +77,7 @@ fn sign_extend<T: BitOrAssign + PrimInt + Signed + WrappingNeg>(
     byte: u8,
 ) {
     // leb128_generic skips the last shift update for efficiency on unsigned ints
+
     if (shift < size_of::<T>() * 8 - 7) && ((byte & 0x40) != 0) {
         // sign extend
         *res |= (T::one() << (shift + 7)).wrapping_neg()
@@ -102,10 +98,63 @@ impl_signed_leb128!(leb128_i128, i128);
 impl_signed_leb128!(leb128_isize, isize);
 
 // TODO: tests
-#[cfg(test)]
+#[cfg(any(test, fuzzing))]
 mod tests {
+    use num_traits::{AsPrimitive, PrimInt, Signed, Unsigned};
+    use arrayvec::ArrayVec;
+    use super::leb128_size;
+
+    // TODO: when https://github.com/rust-lang/rust/issues/43408 is fixed,
+    // write_*_leb128 can use exactly-sized ArrayVecs with const leb128_size
+    const MAX_ENCODED_SIZE: usize = leb128_size::<u128>();
+
+    #[inline]
+    pub fn write_unsigned_leb128<T>(mut n: T) -> ArrayVec<[u8; MAX_ENCODED_SIZE]>
+    where
+        T: AsPrimitive<u8> + PrimInt + Unsigned,
+        u8: AsPrimitive<T>,
+    {
+        let mut vec = ArrayVec::new();
+        loop {
+            if n < 0x80.as_() {
+                vec.push(n.as_());
+                return vec;
+            } else {
+                vec.push((n.as_() & 0x7f) | 0x80);
+                n = n >> 7;
+            }
+        }
+    }
+
+    #[inline]
+    pub fn write_signed_leb128<T>(mut n: T) -> ArrayVec<[u8; MAX_ENCODED_SIZE]>
+    where
+        T: AsPrimitive<u8> + PrimInt + Signed,
+    {
+        let mut vec = ArrayVec::new();
+        loop {
+            let mut byte = n.as_() & 0x7f;
+            n = n >> 7;
+
+            let more = !(((n == T::zero()) && ((byte & 0x40) == 0)) || ((n == -T::one()) && ((byte & 0x40) != 0)));
+            if more {
+                // mark this byte to show that more bytes will follow
+                byte |= 0x80;
+            }
+
+            vec.push(byte);
+
+            if !more {
+                return vec;
+            }
+        }
+    }
+
     #[test]
     fn test() {
         assert_eq!(2 + 2, 4);
     }
 }
+
+#[cfg(fuzzing)]
+pub use tests::{write_signed_leb128, write_unsigned_leb128};
