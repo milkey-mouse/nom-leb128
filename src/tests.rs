6ad6bab6cc29b0e2b4418b89fec67ad0556a2a45
Milkey Mouse <milkeymouse@meme.institute>
2020-06-24 03:00:10 -0700

    Add some "should-fail" tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,14 @@
-use num_traits::{AsPrimitive, PrimInt, Signed, Unsigned};
-use arrayvec::ArrayVec;
 use super::*;
+use arrayvec::ArrayVec;
+use nom::{
+    error::{
+        ErrorKind::TooLarge,
+        VerboseError,
+        VerboseErrorKind::{Context, Nom},
+    },
+    Err::{Error, Incomplete},
+};
+use num_traits::{AsPrimitive, PrimInt, Signed, Unsigned};
 
 // TODO: when https://github.com/rust-lang/rust/issues/43408 is fixed,
 // write_*_leb128 can use exactly-sized ArrayVecs with const leb128_size
@@ -34,7 +42,8 @@ where
         let mut byte = n.as_() & 0x7f;
         n = n >> 7;
 
-        let more = !(((n == T::zero()) && ((byte & 0x40) == 0)) || ((n == -T::one()) && ((byte & 0x40) != 0)));
+        let more = !(((n == T::zero()) && ((byte & 0x40) == 0))
+            || ((n == -T::one()) && ((byte & 0x40) != 0)));
         if more {
             // mark this byte to show that more bytes will follow
             byte |= 0x80;
@@ -55,7 +64,7 @@ macro_rules! test_roundtrip {
             for n in $iter {
                 let encoded = $encoder(n);
                 // TODO: use nom::VerboseError instead of ()
-                let decoded = $decoder::<_, ()>(encoded.as_slice()).unwrap();
+                let decoded = $decoder::<_, VerboseError<_>>(encoded.as_slice()).unwrap();
 
                 assert_eq!(decoded, (&[] as &[u8], n));
             }
@@ -78,9 +87,14 @@ test_bruteforce!(i16, write_signed_leb128, leb128_i16);
 macro_rules! test_tricky_unsigned {
     ($int_ty:ident, $decoder:ident) => {
         // test particularly "tricky" numbers
-        test_roundtrip!($int_ty, write_unsigned_leb128, $decoder, [
-            0, 1, 128, 255, 256, 65535, 65536, $int_ty::MAX
-        ].iter().copied());
+        test_roundtrip!(
+            $int_ty,
+            write_unsigned_leb128,
+            $decoder,
+            [0, 1, 128, 255, 256, 65535, 65536, $int_ty::MAX]
+                .iter()
+                .copied()
+        );
     };
 }
 
@@ -91,10 +105,30 @@ test_tricky_unsigned!(usize, leb128_usize);
 
 macro_rules! test_tricky_signed {
     ($int_ty:ident, $decoder:ident) => {
-        test_roundtrip!($int_ty, write_signed_leb128, $decoder, [
-            0, 1, 128, 255, 256, 65535, 65536, $int_ty::MAX,
-            -1, -128, -255, -256, -65535, -65536, $int_ty::MIN,
-        ].iter().copied());
+        test_roundtrip!(
+            $int_ty,
+            write_signed_leb128,
+            $decoder,
+            [
+                0,
+                1,
+                128,
+                255,
+                256,
+                65535,
+                65536,
+                $int_ty::MAX,
+                -1,
+                -128,
+                -255,
+                -256,
+                -65535,
+                -65536,
+                $int_ty::MIN,
+            ]
+            .iter()
+            .copied()
+        );
     };
 }
 
@@ -104,3 +138,47 @@ test_tricky_signed!(i128, leb128_i128);
 test_tricky_signed!(isize, leb128_isize);
 
 // TODO: add some should_fail tests
+#[test]
+fn data_after_num() {
+    let mut vec = write_unsigned_leb128(1337u32);
+    vec.extend(b"hello".iter().copied());
+
+    let (slice, decoded) = leb128_u32::<_, VerboseError<_>>(vec.as_slice()).unwrap();
+
+    assert_eq!(slice, b"hello");
+    assert_eq!(decoded, 1337);
+}
+
+#[test]
+fn truncated_num() {
+    let mut vec = write_unsigned_leb128(u64::MAX);
+    vec.truncate(2);
+
+    let res = leb128_u32::<_, VerboseError<_>>(vec.as_slice());
+
+    assert_eq!(res, Err(Incomplete(NEED_ONE)));
+}
+
+#[test]
+fn overflow() {
+    let vec = write_unsigned_leb128(u64::MAX);
+
+    let res = leb128_u16::<_, VerboseError<_>>(vec.as_slice());
+
+    if let Err(Error(verbose_error)) = res {
+        assert_eq!(verbose_error.errors[0].1, Nom(TooLarge));
+        assert_eq!(
+            verbose_error.errors[1].1,
+            Context("LEB128 integer is too big to fit in u16")
+        );
+    } else {
+        panic!();
+    }
+}
+
+#[test]
+fn minimal_error() {
+    let vec = write_unsigned_leb128(1337u32);
+
+    leb128_u32::<_, ()>(vec.as_slice()).unwrap();
+}
